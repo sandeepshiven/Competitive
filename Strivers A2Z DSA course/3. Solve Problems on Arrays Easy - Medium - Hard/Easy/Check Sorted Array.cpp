611947d38c3af7bdfc6e9ddbcf498da6e219c07c
sandeepshiven <sandeepshiven0@gmail.com>
2024-04-16 20:59:52 +0530

    added module 3

---
+++ Strivers A2Z DSA course/3. Solve Problems on Arrays Easy - Medium - Hard/Easy/Check Sorted Array.cpp
@@ -0,0 +1,49 @@
+/*
+
+Problem statement
+You have been given an array ‘a’ of ‘n’ non-negative integers.You have to check whether the given array is sorted in the non-decreasing order or not.
+
+
+
+Your task is to return 1 if the given array is sorted. Else, return 0.
+
+
+
+Example :
+Input: ‘n’ = 5, ‘a’ = [1, 2, 3, 4, 5]
+Output: 1
+
+The given array is sorted in non-decreasing order; hence the answer will be 1.
+Detailed explanation ( Input/output format, Notes, Images )
+Sample Input 1 :
+4
+0 0 0 1
+Sample Output 1 :
+1
+Explanation For Sample Input 1 :
+The given array is sorted in non-decreasing order; hence the answer will be 1. 
+Sample Input 2 :
+5
+4 5 4 4 4
+Sample Output 2 :
+0
+Expected Time Complexity:
+O(n), Where ‘n’ is the size of an input array ‘a’.
+Constraints:
+1 ≤ 'n' ≤ 5*10^6
+0 ≤ 'a'[i] ≤ 10^9
+
+Time limit: 1 sec
+
+https://www.naukri.com/code360/problems/ninja-and-the-sorted-check_6581957?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTabValue=PROBLEM
+
+*/
+
+int isSorted(int n, vector<int> arr) {
+    for(int i=0; i<n-1; i++){
+        if(arr[i] > arr[i+1]){
+            return 0;
+        }
+    }
+    return 1;
+}
